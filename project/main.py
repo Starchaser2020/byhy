4d9cafacc01c5ee91e9d9d76aad0d3a149046c80
THY <943919388@qq.com>
2021-10-15 16:31:37 +0800

    2021.10.15

--- project/main.py
+++ project/main.py
@@ -3,5 +3,18 @@ from PySide6.QtGui import *  # type: ignore
 from PySide6.QtWidgets import *  # type: ignore
 from ui_main import Ui_main
 from ui_sign import Ui_sign
+from lib.share import SI
 
 
+class WinMain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_main()
+        self.ui.setupUi(self)
+
+
+app = QApplication()
+app.setStyle('Fusion')
+SI.mainWin = WinMain()
+SI.mainWin.show()
+app.exec()
