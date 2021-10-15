a91f2cf605a228a525d977023e26c0d16b488cac
THY <943919388@qq.com>
2021-10-15 16:08:14 +0800

    2021.10.15

--- project/main.py
+++ project/main.py
@@ -1,5 +1,7 @@
 from PySide6.QtCore import *  # type: ignore
 from PySide6.QtGui import *  # type: ignore
 from PySide6.QtWidgets import *  # type: ignore
+from ui_main import Ui_main
+from ui_sign import Ui_sign
 
 
